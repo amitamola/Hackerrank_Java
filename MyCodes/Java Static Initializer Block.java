0c0e7038058be8ac01641e7f09f211b8722974fc
Amit Amola <amitamola.dun@gmail.com>
2021-05-24 17:44:11 +0530

    Create Java Static Initializer Block.java

---
+++ MyCodes/Java Static Initializer Block.java
@@ -0,0 +1,42 @@
+//https://www.hackerrank.com/challenges/java-static-initializer-block/problem
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    static int B, H;
+    static boolean flag;
+    
+    static
+    {
+        Scanner sc = new Scanner(System.in);
+
+        B = Integer.parseInt(sc.nextLine());
+        H = Integer.parseInt(sc.nextLine());
+    
+        if ((B>0) && (H>0))
+        {
+            flag = true;
+        }
+    
+        else
+        {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+
+    }
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
